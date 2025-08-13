341b730a8f5f0124350f2bcae5d858cdfcf2efcb
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-08-13 20:22:25 -0300

    fix: adiciona booleano validacaoNota

--- AlexandreAtv1.java
+++ AlexandreAtv1.java
@@ -4,11 +4,13 @@ public class AlexandreAtv1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double nota;
+        boolean validacaoNota;
 
         do {
             System.out.println("Digite uma nota de 1 a 10: ");
             nota = scanner.nextDouble();
-            if (nota < 0 || nota > 10) {
+            validacaoNota = nota < 0 || nota > 10;
+            if (validacaoNota) {
                 System.out.println("Nota invalida, digite outra.");
             }
         } while(nota < 0 || nota > 10);
