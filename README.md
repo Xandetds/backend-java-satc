4245b9166ad33b31ed8e40abe1a6e6d20cfc3ac9
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-08-13 20:03:51 -0300

    feat: adiciona arquivos das aulas de backend e exexrcicio 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# backend-java-satc
-Repositório para as aulas de backend em java da UNISATC
---
+++ src/lab/AlexandreAtv1.java
@@ -0,0 +1,13 @@
+package lab;
+import java.util.Scanner;
+
+public class AlexandreAtv1 {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero de 1 a 10: ");
+        String numero = scanner.nextLine();
+
+        System.out.print(numero);
+
+    }
+}
